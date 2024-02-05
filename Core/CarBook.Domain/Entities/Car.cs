5d98e8763200be6beba7698b478f5b1d1a4ff25a
MSD <123319753+msamet07@users.noreply.github.com>
2024-02-06 01:28:57 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int Km { get; set; }
+        public string Transmission { get; set; }
+        public int Seat { get; set; }
+        public int Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigımageUrl { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+    }
+}
